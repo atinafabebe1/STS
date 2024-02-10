5f46989dbee706704dd3dc85a1d6f5f017ed70f4
atinafabebe1 <atinafabebe1@gmail.com>
2024-02-10 17:28:45 +0300

    mongo setup update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,23 +3,21 @@ const mongoose = require('mongoose');
 let dbConnection;
 
 module.exports = {
-    connectToDb: (cb) => {
-        mongoose
-            .connect(process.env.MONGO_URI, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
+    connectToDb: async (cb) => {
+        try {
+            await mongoose.connect(process.env.MONGO_URI, {
+                serverSelectionTimeoutMS: 30000,
                 socketTimeoutMS: 30000,
                 connectTimeoutMS: 30000
-            })
-            .then(() => {
-                dbConnection = mongoose.connection;
-                console.log('Connected to MongoDB');
-                return cb();
-            })
-            .catch((err) => {
-                console.error('Error connecting to MongoDB:', err);
-                return cb(err);
             });
+
+            dbConnection = mongoose.connection;
+            console.log('Connected to MongoDB');
+            cb();
+        } catch (err) {
+            console.error('Error connecting to MongoDB:', err);
+            cb(err);
+        }
     },
     getDb: () => dbConnection
 };
